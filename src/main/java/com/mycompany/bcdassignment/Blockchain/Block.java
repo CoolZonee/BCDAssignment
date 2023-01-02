5bfdc2470f98eefe7ef72c72619eacd7e268f8d2
CoolZonee <ckeat0203@gmail.com>
2023-01-02 18:37:09 +0800

    Done blockchain

---
+++ src/main/java/com/mycompany/bcdassignment/Blockchain/Block.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author coolzone
+ */
+package com.mycompany.bcdassignment.Blockchain;
+
+import com.mycompany.bcdassignment.Hashing.Hasher;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class Block implements Serializable {
+
+    public Header header;
+
+    public Header getHeader() {
+        return header;
+    }
+
+    public TransactionCollection patientRecord;
+
+    public  void setPatientRecord(TransactionCollection patientRecord) {
+        this.patientRecord = patientRecord;
+    }
+
+    @Override
+    public String toString() {
+        return "Block [body=" + patientRecord + ", header=" + header + "]";
+    }
+
+    public Block (String previousHash) {
+        header = new Header();
+        header.setPrevHash(previousHash);
+        header.setTimestamp(new Timestamp(System.currentTimeMillis()).getTime());
+
+        String info = String.join("+", Integer.toString(header.getIndex()), Long.toString(header.getTimestamp()), header.getPrevHash());
+        String blockHash = Hasher.sha256(info);
+        header.setCurrHash(blockHash);
+
+
+    }
+
+    private byte[] getBytes() {
+        try (
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream out = new ObjectOutputStream(baos);
+        ) {
+            out.writeObject(this);
+            return baos.toByteArray();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static class Header implements Serializable {
+        private int index;
+        private String currHash, prevHash;
+        private long timestamp;
+
+        public int getIndex() {
+            return index;
+        }
+
+        public void setIndex(int index) {
+            this.index = index;
+        }
+
+        public String getCurrHash() {
+            return currHash;
+        }
+
+        public void setCurrHash(String currHash) {
+            this.currHash = currHash;
+        }
+
+        public String getPrevHash() {
+            return prevHash;
+        }
+
+        public void setPrevHash(String prevHash) {
+            this.prevHash = prevHash;
+        }
+
+        public long getTimestamp() {
+            return timestamp;
+        }
+
+        public void setTimestamp(long timestamp) {
+            this.timestamp = timestamp;
+        }
+
+        @Override
+        public String toString() {
+            return "Header{" +
+                    "index=" + index +
+                    ", currHash='" + currHash + '\'' +
+                    ", prevHash='" + prevHash + '\'' +
+                    ", timestamp=" + timestamp +
+                    '}';
+        }
+    }
+}
+
---
+++ src/main/java/com/mycompany/bcdassignment/Blockchain/Blockchain.java
@@ -0,0 +1,110 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bcdassignment.Blockchain;
+
+/**
+ *
+ * @author coolzone
+ */
+
+import com.google.gson.GsonBuilder;
+import com.mycompany.bcdassignment.Constant;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.LinkedList;
+
+public class Blockchain {
+    private static LinkedList<Block> db = new LinkedList<>();
+
+    // Singleton pattern
+    private static Blockchain _instance;
+    public static Blockchain getInstance(String moduleName) {
+        if (_instance == null) {
+            _instance = new Blockchain(moduleName);
+        }
+        return _instance;
+    }
+
+    private final String ledgerFileName, chainFileName;
+    private Blockchain (String moduleName) {
+        this.chainFileName = Constant.MASTER_BINARY_DIR + "/" + moduleName + ".bin";
+        this.ledgerFileName = Constant.MASTER_LEDGER_DIR + "/" + moduleName + ".txt";
+    }
+    // Singleton pattern
+
+    /**
+     * genesis()
+     */
+    public void genesis() {
+        Block genesis = new Block("0");
+        db.add(genesis);
+        // persist to the binary file
+        persist();
+        // show ledger
+        distribute();
+    }
+
+    /**
+     * nextBlock()
+     */
+    public void nextBlock(Block newBlock) {
+        // obtain existing blockchain binary
+        db = get();
+        newBlock.getHeader().setIndex(db.getLast().getHeader().getIndex() + 1);
+        db.add(newBlock);
+        // persist to the binary file
+        persist();
+    }
+
+    /**
+     * get()
+     */
+    public LinkedList<Block> get() {
+        try (
+                FileInputStream fis = new FileInputStream(this.chainFileName);
+                ObjectInputStream in = new ObjectInputStream(fis)
+        ) {
+            return (LinkedList<Block>)in.readObject();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * persist()
+     */
+    private void persist() {
+        try (
+                FileOutputStream fos = new FileOutputStream(this.chainFileName);
+                ObjectOutputStream out = new ObjectOutputStream(fos)
+        ) {
+            out.writeObject(db);
+            System.out.println(this.chainFileName + " file is updated!");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * distribute() - display the ledger file
+     */
+    public void distribute() {
+        // convert the chain to String using gson api
+        try {
+            String chain = new GsonBuilder().setPrettyPrinting().create().toJson(db);
+            System.out.println(chain);
+
+            // write to file
+            Files.write(Paths.get(this.ledgerFileName), chain.getBytes(), StandardOpenOption.CREATE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
